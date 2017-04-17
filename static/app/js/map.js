e6621f49c5a7b00febb4dcd7af9a0b4360a3f23e
Zontzip <zontzor@gmail.com>
2017-04-17 22:58:41 +0100

    Refactor map js

--- static/app/js/map.js
+++ static/app/js/map.js
@@ -1,23 +1,23 @@
-const map = L.map('map').setView([53.3, -6.3], 5);
-const friends_list = document.getElementsByClassName("friend-list-item");
-var myLatLon;
+// Create user geo constants
+const mygeom = JSON.parse($("#geom").html());
+const myLatLon = L.latLng([mygeom.coordinates[1], mygeom.coordinates[0]]);
 
+// Create map
+const map = L.map('map').setView([53.3, -6.3], 5);
 L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoiem9udHppcCIsImEiOiJjajFtbGJrYjEwMDAxMzNvdGs4OXByM2dhIn0.hzPQNENTIuVgt7fPXsUD5Q', {})
     .addTo(map);
 
-map_init();
-
+// Init friends list geo
+const friends_list = document.getElementsByClassName("friend-list-item");
 for (var i = 0; i < friends_list.length; i++) {
     friends_list[i].addEventListener('click', function(e) {
         map_set(this);
     })
 }
 
-function map_init() {
-    var geom = JSON.parse($("#geom").html());
-    myLatLon = L.latLng([geom.coordinates[1], geom.coordinates[0]]);
+map_init();
 
-    geom = L.geoJson(geom);
+function map_init() {
     var marker = L.marker(myLatLon)
         .addTo(map)
         .bindPopup("Me")
@@ -30,13 +30,8 @@ function map_set(element) {
     var geom = JSON.parse(element.dataset.location);
     var LatLon = L.latLng([geom.coordinates[1], geom.coordinates[0]]);
     var distance = LatLon.distanceTo(myLatLon) < 1000 ? Math.round(LatLon.distanceTo(myLatLon)) + ' m' : Math.round(LatLon.distanceTo(myLatLon) / 1000) + ' km';
+    var info = "<dl><dt>" + element.dataset.firstname + ' ' + element.dataset.lastname + "</dt>" + "<dd>" + distance + "</dd>";
 
-    var name = element.dataset.firstname + ' ' + element.dataset.lastname;
-
-    var info = "<dl><dt>" + name + "</dt>"
-           + "<dd>" + distance + "</dd>";
-
-    geom = L.geoJson(geom);
     var marker = L.marker(LatLon)
         .addTo(map)
         .bindPopup(info)
