5a97b8919e73a345c3fdb79edf941425a7704c1d
Zontzip <zontzor@gmail.com>
2017-04-17 22:20:03 +0100

    Add marker popup

--- static/app/js/map.js
+++ static/app/js/map.js
@@ -4,28 +4,36 @@ const friends_list = document.getElementsByClassName("friend-list-item");
 L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoiem9udHppcCIsImEiOiJjajFtbGJrYjEwMDAxMzNvdGs4OXByM2dhIn0.hzPQNENTIuVgt7fPXsUD5Q', {})
     .addTo(map);
 
+map_init();
+
+for (var i = 0; i < friends_list.length; i++) {
+    friends_list[i].addEventListener('click', function(e) {
+        map_set(this);
+    })
+}
+
 function map_init() {
     var geom = JSON.parse($("#geom").html());
     var myLatLon = L.latLng([geom.coordinates[1], geom.coordinates[0]]);
 
     geom = L.geoJson(geom);
-    geom.addTo(map);
+    var marker = L.marker(myLatLon)
+        .addTo(map)
+        .bindPopup("Me")
+        .openPopup();
 
     map.setView(myLatLon, 16);
 }
 
 function map_set(element) {
     var geom = JSON.parse(element.dataset.location);
-    var myLatLon = L.latLng([geom.coordinates[1], geom.coordinates[0]]);
+    var LatLon = L.latLng([geom.coordinates[1], geom.coordinates[0]]);
 
     geom = L.geoJson(geom);
-    geom.addTo(leafletmap);
+    var marker = L.marker(LatLon)
+        .addTo(map)
+        .bindPopup(element.dataset.firstname + ' ' + element.dataset.lastname)
+        .openPopup();
 
-    leafletmap.setView(myLatLon, 16);
-}
-
-for (var i = 0; i < friends_list.length; i++) {
-    friends_list[i].addEventListener('click', function(e) {
-        map_set(this);
-    })
+    map.setView(LatLon, 16);
 }
