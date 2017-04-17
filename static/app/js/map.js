a5133784bcbffb68b3bde707fb0110085bde2cfb
Zontzip <zontzor@gmail.com>
2017-04-17 22:38:28 +0100

    Add name and distance to marker

--- static/app/js/map.js
+++ static/app/js/map.js
@@ -1,5 +1,6 @@
 const map = L.map('map').setView([53.3, -6.3], 5);
 const friends_list = document.getElementsByClassName("friend-list-item");
+var myLatLon;
 
 L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoiem9udHppcCIsImEiOiJjajFtbGJrYjEwMDAxMzNvdGs4OXByM2dhIn0.hzPQNENTIuVgt7fPXsUD5Q', {})
     .addTo(map);
@@ -14,7 +15,7 @@ for (var i = 0; i < friends_list.length; i++) {
 
 function map_init() {
     var geom = JSON.parse($("#geom").html());
-    var myLatLon = L.latLng([geom.coordinates[1], geom.coordinates[0]]);
+    myLatLon = L.latLng([geom.coordinates[1], geom.coordinates[0]]);
 
     geom = L.geoJson(geom);
     var marker = L.marker(myLatLon)
@@ -28,11 +29,17 @@ function map_init() {
 function map_set(element) {
     var geom = JSON.parse(element.dataset.location);
     var LatLon = L.latLng([geom.coordinates[1], geom.coordinates[0]]);
+    var distance = LatLon.distanceTo(myLatLon) < 1000 ? Math.round(LatLon.distanceTo(myLatLon)) + ' m' : Math.round(LatLon.distanceTo(myLatLon) / 1000) + ' km';
+
+    var name = element.dataset.firstname + ' ' + element.dataset.lastname;
+
+    var info = "<dl><dt>" + name + "</dt>"
+           + "<dd>" + distance + "</dd>";
 
     geom = L.geoJson(geom);
     var marker = L.marker(LatLon)
         .addTo(map)
-        .bindPopup(element.dataset.firstname + ' ' + element.dataset.lastname)
+        .bindPopup(info)
         .openPopup();
 
     map.setView(LatLon, 16);
